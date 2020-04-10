186ddb0bc1ba71e3a8b50b33a8fbc8b19756b83c
babachuan <1029155580@qq.com>
2020-04-10 22:24:20 +0800

    eureka registry

---
+++ Excercise/micoserve/register/eurekaprovider/src/main/java/com/example/eurekaprovider/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.eurekaprovider.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/getHi")
+    public String getHi(){
+
+        return "hello";
+    }
+}
